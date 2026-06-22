ae7cc73798cb6a332398acce5f105f735ba8f343
justin <evajusro@gmail.com>
2026-06-22 09:57:53 -0400

    various changes

--- player.gd
+++ player.gd
@@ -52,7 +52,7 @@ func _unhandled_input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("cheat_tower_1"):
 		tower_placer.create_temp_tower(load("res://towers/scenes/mrmouse.tscn").instantiate( ))
 	if Input.is_action_just_pressed("cheat_tower_2"):
-		tower_placer.create_temp_tower(load("res://towers/scenes/specimen.tscn").instantiate( ))
+		tower_placer.create_temp_tower(load("res://towers/scenes/pirate.tscn").instantiate( ))
 
 func spend_money(amount : int) -> bool:
 	if money >= amount:
