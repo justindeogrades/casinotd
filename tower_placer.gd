b8c70618385dc6f662c13e2bb0e2fa41eff75f50
justin <evajusro@gmail.com>
2026-06-23 15:35:17 -0400

    reworked the tower placer code, added an indicator for an invalid tower placement position

--- tower_placer.gd
+++ tower_placer.gd
@@ -19,8 +19,15 @@ signal tower_placed(tower : Tower)
 	#tower_ghost.init(tower)
 
 func _process(delta : float) -> void:
+	if tower_ghost != null:
+		if get_tower_placement_validity(tower_ghost.position):
+			tower_ghost.modulate = Color(1, 1, 1, 1)
+		else:
+			tower_ghost.modulate = Color(1, 0, 0, 1)
+	
 	if Input.is_action_just_pressed("place_tower") and tower_ghost != null:
-		place_tower(get_viewport().get_mouse_position())
+		if get_tower_placement_validity(tower_ghost.position):
+			place_tower(get_viewport().get_mouse_position())
 		#new_tower.mousebox/$CollisionShape2D.mouse_entered.connect(_on_tower_mouse_entered)
 		#new_tower.mousebox/$CollisionShape2D.mouse_exited.connect(_on_tower_mouse_exited)
 		#if new_tower != null:
@@ -53,22 +60,27 @@ func create_tower(tower_to_place : Tower, pos : Vector2):
 
 #Why does this return a value?
 func place_tower(pos : Vector2) -> Tower:
-	#tower_to_place = tower_preload.instantiate()
-	#add_child(tower_to_place)
-	#tower_to_place.position = get_viewport().get_mouse_position()
-	#return tower_to_place
-	if get_parent().is_any_tower_mouseovered():
-		return null
-	
+	#if get_parent().is_any_tower_mouseovered():
+		#return null
+	#
 	#if not Geometry2D.is_point_in_polygon(pos, track_polygon.polygon):
-		#tower_to_place = tower_preload.instantiate()
-		#add_child(tower_to_place)
-		#tower_to_place.position = pos
-		#return tower_to_place
+		#temp_tower.position = pos
+		#tower_ghost.queue_free()
+		#tower_placed.emit(temp_tower)
+		#return temp_tower
+	#return null
 	
-	if not Geometry2D.is_point_in_polygon(pos, track_polygon.polygon):
-		temp_tower.position = pos
-		tower_ghost.queue_free()
-		tower_placed.emit(temp_tower)
-		return temp_tower
-	return null
+	#We check for placement validity in the process function
+	temp_tower.position = pos
+	tower_ghost.queue_free()
+	tower_placed.emit(temp_tower)
+	return temp_tower
+
+func get_tower_placement_validity(pos : Vector2) -> bool:
+	if Geometry2D.is_point_in_polygon(pos, track_polygon.polygon):
+		return false
+	if get_parent().is_any_tower_mouseovered():
+		return false
+	if pos.x >= get_viewport().get_visible_rect().size.x * 0.8:
+		return false
+	return true
